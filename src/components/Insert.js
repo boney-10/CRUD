1a76bc761ec59bf3cfc7fb773ac309fc60bbd33f
Boney <boney11717102@gmail.com>
2022-02-13 20:32:38 +0000

    Done

---
+++ src/components/Insert.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+export default class Insert extends Component {
+  render() {
+    return (
+      <div>
+        <p>I am in Insert!</p>
+      </div>
+    )
+  }
+}
