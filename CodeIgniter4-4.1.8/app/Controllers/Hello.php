149df7eee420ff33de9c27671d3eb920a5f20545
Boney <boney11717102@gmail.com>
2022-02-26 22:37:00 +0000

    update

---
+++ CodeIgniter4-4.1.8/app/Controllers/Hello.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Hello extends BaseController
+{
+    public function index()
+    {
+        echo 'Hello ci-4sd';
+    }
+    public function document(){
+      echo 'Hello ci-4sdvsdgdsfs';
+    }
+}
